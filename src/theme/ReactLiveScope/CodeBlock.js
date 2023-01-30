ea3e5e7f010dae4db47a772474a4eb56ef467eee
Zobeir Hamid <zobeirhamid@berkeley.edu>
2023-01-30 02:20:42 -0800

    chore: bump version

--- src/theme/ReactLiveScope/CodeBlock.js
+++ src/theme/ReactLiveScope/CodeBlock.js
@@ -4,7 +4,7 @@ import { ZeroKitProvider } from "@zerodevapp/zerokit";
 
 export default function WrappedCodeBlock(props) {
   return (
-    <ZeroKitProvider projectId="f5359ea1-5124-4051-af8f-220f34bf2f59" modalSize="compact">
+    <ZeroKitProvider projectId="f5359ea1-5124-4051-af8f-220f34bf2f59">
       <CodeBlock {...props} />
     </ZeroKitProvider>
   );
