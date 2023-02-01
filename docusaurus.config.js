eb7beb53eb382b2a4ca44ab2d1f514287eba1659
Zobeir Hamid <zobeirhamid@berkeley.edu>
2023-02-01 09:33:39 -0800

    chore: update url and removes github link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docs.zerokit.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -89,11 +89,11 @@ const config = {
             label: 'Documentation',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/zerodevapp/zerokit',
-            label: 'GitHub',
-            position: 'right',
-          },
+          // {
+          //   href: 'https://github.com/zerodevapp/zerokit',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
         ],
       },
       prism: {
