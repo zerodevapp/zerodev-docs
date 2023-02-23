28f6e1e74ca751f2a750992f1153af51829da703
Zobeir Hamid <zobeirhamid@berkeley.edu>
2023-02-23 13:59:08 -0800

    fix: remove social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/zerokit-social-card.jpg',
+      // image: 'img/zerokit-social-card.jpg',
       navbar: {
         title: 'ZeroDev',
         logo: {
